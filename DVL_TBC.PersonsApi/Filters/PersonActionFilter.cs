90057f456ab05cc1c7407c9e397439db578ab496
DVL_KinoMan <dvali.daviti@gmail.com>
2020-12-01 19:25:07 +0400

    AgregateException on EditRequest

--- DVL_TBC.PersonsApi/Filters/PersonActionFilter.cs
+++ DVL_TBC.PersonsApi/Filters/PersonActionFilter.cs
@@ -1,4 +1,6 @@
-﻿using DVL_TBC.PersonsApi.Attributes;
+﻿using System;
+using System.Collections.Generic;
+using DVL_TBC.PersonsApi.Attributes;
 using DVL_TBC.PersonsApi.Models;
 using DVL_TBC.PersonsApi.Resources;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -19,43 +21,50 @@ namespace DVL_TBC.PersonsApi.Filters
             if (context.ActionArguments.TryGetValue("editPersonRequest", out var editRequest) &&
                 editRequest is EditPersonRequest { } ePerRequest)
             {
+                var validationErrors = new List<ValidationException>();
+
                 if (ePerRequest.FirstName is { } firstName)
                 {
                     if (!ValidLength(firstName, 2, 50))
-                        throw new ValidationException(string.Format(Translations.ErrorParameterLengthRange,
-                            nameof(ePerRequest.FirstName), 2, 50));
+                        validationErrors.Add(new ValidationException(string.Format(
+                            Translations.ErrorParameterLengthRange,
+                            nameof(ePerRequest.FirstName), 2, 50)));
 
                     if (!GeoOrLatinCharactersAttribute.IsValid(firstName))
-                        throw new ValidationException(
-                            $"{Translations.ErrorAllLettersGeoOrLatin} (Param: {nameof(ePerRequest.FirstName)} ParamValue: {firstName})");
+                        validationErrors.Add(new ValidationException(
+                            $"{Translations.ErrorAllLettersGeoOrLatin} (Param: {nameof(ePerRequest.FirstName)} ParamValue: {firstName})"));
                 }
 
                 if (ePerRequest.LastName is { } lastName)
                 {
                     if (!ValidLength(lastName, 2, 50))
-                        throw new ValidationException(string.Format(Translations.ErrorParameterLengthRange,
-                            nameof(ePerRequest.LastName), 2, 50));
+                        validationErrors.Add(new ValidationException(string.Format(Translations.ErrorParameterLengthRange,
+                            nameof(ePerRequest.LastName), 2, 50)));
 
                     if (!GeoOrLatinCharactersAttribute.IsValid(lastName))
-                        throw new ValidationException(
-                            $"{Translations.ErrorAllLettersGeoOrLatin} (Param: {nameof(ePerRequest.LastName)} ParamValue: {lastName})");
+                        validationErrors.Add(new ValidationException(
+                            $"{Translations.ErrorAllLettersGeoOrLatin} (Param: {nameof(ePerRequest.LastName)} ParamValue: {lastName})"));
                 }
 
                 if (ePerRequest.BirthDate is { } birthDate && !BirthDateAttribute.IsValid(birthDate, 18))
-                    throw new ValidationException(string.Format(Translations.ErrorMinAgeShouldBe, 18));
+                    validationErrors.Add(new ValidationException(string.Format(Translations.ErrorMinAgeShouldBe, 18)));
 
                 if (ePerRequest.PrivateNumber is { } privateNumber)
                 {
                     if (!PrivateNumberAttribute.HasValidLength(privateNumber, 11))
-                        throw new ValidationException(string.Format(Translations.ErrorPrivateNumberLength, 11));
+                        validationErrors.Add(new ValidationException(string.Format(Translations.ErrorPrivateNumberLength, 11)));
 
                     if (!PrivateNumberAttribute.IsValid(privateNumber))
-                        throw new ValidationException(Translations.ErrorPrivateNumberOnlyDigits);
+                        validationErrors.Add(new ValidationException(Translations.ErrorPrivateNumberOnlyDigits));
                 }
 
                 if (ePerRequest.PhoneNumbers is { } phoneNumbs &&
                     phoneNumbs.Any(numb => !ValidLength(numb.Number, 4, 50)))
-                    throw new ValidationException(Translations.ErrorPhoneNumberRange);
+                    validationErrors.Add(new ValidationException(Translations.ErrorPhoneNumberRange));
+
+                if (validationErrors.Count != 0)
+                    throw new ValidationException("EditPersonRequest Validation failed.",
+                        new AggregateException(validationErrors));
             }
 
             static bool ValidLength(string val, int minLength, int maxLength) =>
